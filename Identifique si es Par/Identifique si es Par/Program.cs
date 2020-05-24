e688a10276d2a0370cf38bffebf2d576f2a3de2c
Escarllet <Escarllet12@gmail.com>
2020-05-23 21:16:03 -0500

    Identifique si es par. version 2.0

--- Identifique si es Par/Identifique si es Par/Program.cs
+++ Identifique si es Par/Identifique si es Par/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Identifique_si_es_Par
+namespace mandato_1_diga_si_es_par
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int n1, n2;
+            Console.WriteLine("Introduzca un numero");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = n1 % 2;
+            if (n2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+
+            }
         }
     }
 }
