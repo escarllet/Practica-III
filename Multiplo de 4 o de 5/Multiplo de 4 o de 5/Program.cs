2e91fdece491ec7fdded64d9c3599f7d4fda2cfd
Escarllet <Escarllet12@gmail.com>
2020-05-23 22:05:16 -0500

    Diga si es multiplo de 4 y/o de 5. version 1.0

---
+++ Multiplo de 4 o de 5/Multiplo de 4 o de 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplo_de_4_o_de_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, resto1, resto2;
+            Console.WriteLine("Escriba un numero");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            resto1 = n1 % 4;
+            resto2 = n1 % 5;
+            if (resto1 == 0)
+            {
+                Console.WriteLine(n1 +" es multiplo de 4");
+            }
+            if (resto2 == 0) 
+            {
+                Console.WriteLine(n1 + " es multiplo de 5");
+            }
+            // Decidi usar dos if en vez de un if y un else if, porque un numero puede ser multiplo de 4 y de 5 al mismo tiempo.
+        }
+    }
+}
