59a933a5d3e3d74e9fa20b22bb899dabd0bff6bf
Escarllet <Escarllet12@gmail.com>
2020-05-23 22:29:51 -0500

    Identifique al mayor de 3 numeros reales. version 1.0

---
+++ Mayor de 3 numeros reales/Mayor de 3 numeros reales/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mayor_de_3_numeros_reales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3;
+            //double y float son tipos de variables numericas reales
+            Console.WriteLine("Ingrese un numero");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el ultimo numero");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("El numero mayor es: " + n1);
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine("El numero mayor es: " + n2);
+            }
+            else if (n3 > n2 && n3 > n1)
+            {
+                Console.WriteLine("El numero mayor es: " + n3);
+            }
+            Console.ReadKey();
+        }
+    }
+}
