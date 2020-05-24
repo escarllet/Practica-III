f81f8d3a4f0124035f542795d8e3f3f24f860f62
Escarllet <Escarllet12@gmail.com>
2020-05-23 21:25:24 -0500

    Identifique si un numero es multiplo de otro. version 1.0

---
+++ Un numero es multiplo del otro/Un numero es multiplo del otro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Un_numero_es_multiplo_del_otro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, resto;
+            Console.WriteLine("Escriba un numero");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba otro numero");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            resto = n1 % n2;
+            if (resto == 0)
+            {
+                Console.WriteLine(n1+" es multiplo de "+n2);
+            }
+            else
+            {
+                Console.WriteLine(n1 + " NO es multiplo de " + n2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
