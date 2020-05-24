3998725709d90ebbefb648062ed5581d81643656
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-05-23 20:49:35 -0500

    Identifique si es par. version 1.0

---
+++ Identifique si es Par/Identifique si es Par/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identifique_si_es_Par
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
