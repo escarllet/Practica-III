1f62aa79daf09776a30269de21de1ddf52fbdc81
Escarllet <Escarllet12@gmail.com>
2020-05-23 21:07:10 -0500

    Mayor de dos numeros enteros. version 1.0

---
+++ Mayor de dos numeros enteros/Mayor de dos numeros enteros/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mayor_de_dos_numeros_enteros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Ingrese un numero");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine(n1 + " es el numero mayor");
+            }
+            else if (n1 < n2)
+            {
+                Console.WriteLine(n2 + " es el numero mayor");
+            }
+            else { Console.WriteLine("Los numeros son iguales"); }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mayor de dos numeros enteros/Mayor de dos numeros enteros/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Mayor de dos numeros enteros")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Mayor de dos numeros enteros")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("6d507ab7-1d0e-4333-a090-b91d399e6706")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
